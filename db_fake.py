145d1ae1fb0d11c4696db99f8282ce6ceeff9bff
huxuan <i@huxuan.org>
2015-04-18 15:47:42 +0800

    Add db_fake.

---
+++ db_fake.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: db_fake.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: Script to generate fake database.
+"""
+from app import app
+from app import models
+
+def fake_client():
+    models.Client.setter('fangmi-web')
+
+def fake_user():
+    models.User.setter('u1', 'pwd1')
+    models.User.setter('u2', 'pwd2')
+
+def main():
+    fake_client()
+    fake_user()
+
+if __name__ == '__main__':
+    main()
