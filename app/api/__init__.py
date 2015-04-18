d9bcde4bedff259f31317d680d7fff8ad2da8e43
huxuan <i@huxuan.org>
2015-04-18 21:15:17 +0800

    APIResponse and APIException.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -6,14 +6,63 @@ Author: huxuan
 Email: i(at)huxuan.org
 Description: Init file for api blueprint.
 """
-from flask import Blueprint
+from functools import partial
 
-from .. import app
-from app import models
+from flask import Blueprint
+from flask import Flask
+from flask import jsonify
+
+from .. import api
+from .. import models
+from .. import oauth
+import code
 
 bp_api = Blueprint('api', __name__)
 
+# NOTE(huxuan): Use Flask's error handler instead of the one in flask-restful.
+bp_api.handle_exception = partial(Flask.handle_exception, bp_api)
+bp_api.handle_user_exception = partial(Flask.handle_user_exception, bp_api)
+
+
+class APIResponse():
+
+    def __init__(self, status_code=None, message=None, payload=None):
+        self.status_code = status_code or code.API_CODE_OK
+        self.message = message or code.API_CODE_MESSAGE.get(
+            self.status_code, u'Invalid status code.')
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        if self.message is not None:
+            rv['message'] = self.message
+        rv['status_code'] = self.status_code
+        return rv
+
+
+class APIException(Exception, APIResponse):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self)
+        APIResponse.__init__(self, *args, **kwargs)
+
+
+@bp_api.errorhandler(APIException)
+def error_handler(api_exception):
+    return jsonify(api_exception.to_dict())
+
+
+def api_response(*args, **kwargs):
+    return jsonify(APIResponse(*args, **kwargs).to_dict())
+
+
 @bp_api.route('/')
 @bp_api.route('/test')
+@bp_api.route('/test/response')
+def response():
+    return api_response(message='APIResponse from {}!'.format(__name__))
+
+
+@bp_api.route('/test/exception')
 def test():
-    return 'Hello world from {}!'.format(__name__)
+    raise APIException(message='APIException from {}!'.format(__name__))
---
+++ app/api/code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File: errors.py
+Author: huxuan
+Email: i(at)huxuan.org
+Description: API status code definition.
+"""
+
+API_CODE_OK = 200
+
+API_CODE_MESSAGE = {
+    API_CODE_OK: u'OK',
+}
